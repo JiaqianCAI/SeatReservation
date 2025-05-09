b04b332e934639e328ae3676825141ee291aa5b8
Sissi CAI <jiaqian.cai@student.uts.edu.au>
2025-05-09 19:23:01 +1000

    Add files via upload

---
+++ SeatReservation/SeatReservation/SeatReservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeatReservationApp.swift
+//  SeatReservation
+//
+//  Created by Jiaqian Cai on 9/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct SeatReservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
